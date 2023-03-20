d6ddd675e08446ebf83fdb7c118cfcb9ebdb9e3f
beshoy <besho.elking33377@gmail.com>
2023-03-20 05:48:37 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C while function
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar(x + '0');
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
