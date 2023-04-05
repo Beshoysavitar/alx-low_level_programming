d0c948237e6a34ac42ba270a5eae9d3387f4da5d
beshoy <besho.elking33377@gmail.com>
2023-04-05 12:37:54 -0700

    0x08-recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,11 +10,9 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (*s < '\0')
+	if (*s > '\0')
 	{
-		_putchar(*s);
-		_strlen_recursion(s + 1);
-		i++;
+		i += _strlen_recursion(s + 1) + 1;
 	}
 	return (i);
 }
