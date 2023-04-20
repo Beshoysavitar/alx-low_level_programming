69a451c0aab3b34dfa82ee63188bbf1315b677b6
beshoy <besho.elking33377@gmail.com>
2023-04-20 13:59:29 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - a function prints numbers followed by a new line
+ * print_strings - a function prints numbers followed by a new line
  * @n: number of parameter
  * @separator: separator between two numbers
  * Return: Always 0
