a1f5cf91941292a4dc80942a89a7667e9cfcb5ec
beshoy <besho.elking33377@gmail.com>
2023-04-20 13:56:37 -0700

    0x10-variadic_functions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - a function prints numbers followed by a new line
+ * @n: number of parameter
+ * @separator: separator between two numbers
+ * Return: Always 0
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int j;
+	va_list a;
+	char *s
+
+	if (separator == NULL)
+		separator = "";
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(a, n);
+	for (j = 0; j < n; j++)
+	{
+		s = va_arg(ap, char*);
+		if (s == NULL)
+			s = "(nil)";
+		printf("%s", s);
+		if (j < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(a);
+}
