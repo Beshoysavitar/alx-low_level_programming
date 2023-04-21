3de3765a14a157566a01771f8c6579dfe76e9c73
beshoy <besho.elking33377@gmail.com>
2023-04-21 09:31:50 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,1 +1,29 @@
-#
+#include "function_pointers.h"
+/**
+ * main - ...
+ * @argc: ...
+ * @argv: ...
+ *
+ * Return: ...
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	char *s = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	i = atoi(argv[1]);
+	if (i < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	while (i--)
+		printf("%02hhx%s", *s++, i ? " " : "\n");
+	return (0);
+}
+
