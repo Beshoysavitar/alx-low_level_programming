88043783a3f30b81e269721f44b1f8e277d2f84f
beshoy <besho.elking33377@gmail.com>
2023-05-02 12:32:56 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len - function that returns the number of elements
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h);
+{
+	size_t nod;
+
+	for (nod = 0; h; nod++)
+	{
+		h = h->next;
+	}
+
+	return (nod);
+}
