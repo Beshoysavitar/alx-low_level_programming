97bec53a5eafd38ff59dcd0561511958c5aaf559
beshoy <besho.elking33377@gmail.com>
2023-05-02 18:10:56 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - ...
+ * @head: ...
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *i;
+
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		i = head;
+		head = head->next;
+		free(i);
+	}
+}
