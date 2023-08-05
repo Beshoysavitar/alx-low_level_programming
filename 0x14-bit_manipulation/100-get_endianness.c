70580dd3bbc6006bd20590047a249f04d5f2b831
beshoy <besho.elking33377@gmail.com>
2023-08-04 18:23:27 -0700

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - ***
+ *
+ * Return: 0 if big endian, 1 if small
+ */
+int get_endianness(void)
+{
+	unsigned int q = 1;
+	char *i = (char*) &q;
+
+	 return (*i);
+}
