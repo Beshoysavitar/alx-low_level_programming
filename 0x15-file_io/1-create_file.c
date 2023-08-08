79cadc966c17f4b99d8cc5eaaa1e356788929c44
beshoy <besho.elking33377@gmail.com>
2023-08-07 17:45:31 -0700

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,38 +8,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t n_written;
+	int o, w, len = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-    if (text_content != NULL)
-    {
-        n_written = write(fd, text_content, _strlen(text_content));
-        if (n_written == -1)
-            return (-1);
-    }
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-    close(fd);
-    return (1);
-}
+	if (o == -1 || w == -1)
+		return (-1);
 
-/**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-size_t _strlen(char *s)
-{
-    size_t i;
+	close(o);
 
-    for (i = 0; s[i]; i++)
-        ;
-    return (i);
+	return (1);
 }
