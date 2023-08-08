9f640f0343918a8e3f2888bd45a9f83621f56592
beshoy <besho.elking33377@gmail.com>
2023-08-07 17:06:28 -0700

    0x15-file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * read_textfile - ***
+ * @filename: ***
+ * @letters: ***
+ * Return: ***
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t n_r, n_w;
+	int f;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	f = open(filename, O_RDONLY);
+
+	if (f == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+
+	if (buf == NULL)
+		return (0);
+
+	n_r = read(f, buf, letters);
+
+	if (n_r == -1)
+		return (0);
+
+	n_w = write(STDOUT_FILENO, buf, n_r);
+
+	if (n_w == -1 || n_w != n_r)
+		return (0);
+
+	free(buf);
+	close(fd);
+	return (n_w);
+}
