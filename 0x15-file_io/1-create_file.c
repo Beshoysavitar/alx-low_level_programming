6fb5dbca1f95f81ec3a8ae7e787310cb071bb19f
beshoy <besho.elking33377@gmail.com>
2023-08-07 17:37:41 -0700

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,6 +18,7 @@ int create_file(const char *filename, char *text_content)
 		for (l = 0; text_content[l];)
 			l++;
 	}
+
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, l);
 
@@ -25,6 +26,5 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	close(op);
-
 	return (1);
 }
