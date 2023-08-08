1787612baafea9a73a9e73bf6d900dc8d0f40364
beshoy <besho.elking33377@gmail.com>
2023-08-07 18:25:47 -0700

    0x15-file_io

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,52 +5,37 @@
 #include <elf.h>
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: elf_header elf_filename\n");
-    exit(1);
-  }
+if (argc != 2) {
+fprintf(stderr, "Usage: elf_header elf_filename\n");
+exit(1);
+}
 
-  char *filename = argv[1];
-  int fd = open(filename, O_RDONLY);
-  if (fd < 0) {
-    perror("open");
-    exit(1);
-  }
+char *filename = argv[1];
+int fd = open(filename, O_RDONLY);
+if (fd < 0) {
+perror("open");
+exit(1);
+}
 
-  Elf64_Ehdr elf_header;
+Elf64_Ehdr elf_header;
+if (memcmp(elf_header.e_ident, ELFMAG, 4) != 0) {
+fprintf(stderr, "File is not an ELF file\n");
+exit(98);
+}
+printf("ELF Header:\n");
+printf("  Magic: ");
+for (int i = 0; i < EI_NIDENT; i++) {
+printf("%#x ", elf_header.e_ident[i]);
+}
+printf("\n");
+printf("  Class: %d\n", elf_header.e_class);
+printf("  Data: %d\n", elf_header.e_data);
+printf("  Version: %d\n", elf_header.e_version);
+printf("  OS/ABI: %#x\n", elf_header.e_osabi);
+printf("  ABI Version: %d\n", elf_header.e_abiversion);
+printf("  Type: %d\n", elf_header.e_type);
+printf("  Entry point address: %#x\n", elf_header.e_entry);
 
-  // Check if the file is an ELF file
-  if (elf_header.e_ident[EI_MAG0] != ELFMAG0 ||
-      elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
-      elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
-      elf_header.e_ident[EI_MAG3] != ELFMAG3) {
-    fprintf(stderr, "File is not an ELF file\n");
-    exit(98);
-  }
-
-  // Seek to the start of the ELF header
-  if (lseek(fd, 0, SEEK_SET) != 0) {
-    perror("lseek");
-    exit(1);
-  }
-
-  // Read the ELF header
-  if (read(fd, &elf_header, sizeof(elf_header)) != sizeof(elf_header)) {
-    perror("read");
-    exit(1);
-  }
-
-  // Print the ELF header information
-  printf("ELF Header:\n");
-  printf("  Magic: %#x\n", elf_header.e_ident[EI_MAG0]);
-  printf("  Class: %d\n", elf_header.e_class);
-  printf("  Data: %d\n", elf_header.e_data);
-  printf("  Version: %d\n", elf_header.e_version);
-  printf("  OS/ABI: %#x\n", elf_header.e_osabi);
-  printf("  ABI Version: %d\n", elf_header.e_abiversion);
-  printf("  Type: %d\n", elf_header.e_type);
-  printf("  Entry point address: %#x\n", elf_header.e_entry);
-
-  close(fd);
-  return 0;
+close(fd);
+return 0;
 }
